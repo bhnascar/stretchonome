6b1595214be1fa68bd8f17127a76f02bc775e6c1
bhnascar <bhnascar@gmail.com>
2021-01-28 02:17:47 -0800

    Interface tweaks.

--- timeline.js
+++ timeline.js
@@ -26,17 +26,16 @@ class Timeline {
     this.ctx.scale(dpr, dpr);
 
     this.ctx.font = '12px sans-serif';
-    this.ctx.strokeStyle = '#999';
-    this.ctx.fillStyle = '#999';
+    this.ctx.strokeStyle = '#666';
+    this.ctx.fillStyle = '#666';
     this.ctx.lineWidth = 2;
   }
 
   toDateString(time) {
     const tmp = new Date(time * 1000);
-    const hours = tmp.getUTCHours().toString().padStart(2, '0');
     const minutes = tmp.getUTCMinutes().toString().padStart(2, '0');
     const seconds = tmp.getSeconds().toString().padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
+    return `${minutes}:${seconds}`;
   }
 
   drawTicks(time) {
@@ -44,24 +43,26 @@ class Timeline {
     const left = time - 0.5 * this.windowSize;
     const right = time + 0.5 * this.windowSize;
     let cur = Math.ceil(left / this.minorInterval) * this.minorInterval;
+    const majorLineHeight = 0.12 * this.canvas.clientHeight;
+    const minorLineHeight = 0.08 * this.canvas.clientHeight;
     while (cur < right) {
       if (cur >= 0) {
         const x = ((cur - left) / this.windowSize) * this.canvas.clientWidth;
         const isMajorInterval = (cur / this.majorInterval) - Math.floor(cur / this.majorInterval) < 0.001;
-        const lineHeight = (isMajorInterval) ? 75.5 : 50.5;
+        const lineHeight = (isMajorInterval) ? majorLineHeight : minorLineHeight;
         this.ctx.beginPath();
         this.ctx.moveTo(x, 0);
         this.ctx.lineTo(x, lineHeight);
         this.ctx.stroke();
         if (isMajorInterval) {
-          this.ctx.fillText(this.toDateString(cur), x + 5, 73.5);
+          this.ctx.fillText(this.toDateString(cur), x + 5, majorLineHeight - 2);
         }
       }
       cur += this.minorInterval;
     }
   }
 
-  drawBeats(time) {
+  drawBeats(time, beats) {
     const left = time - 0.5 * this.windowSize;
     const right = time + 0.5 * this.windowSize;
     
@@ -74,6 +75,8 @@ class Timeline {
     }
 
     let reuseIndex = 0;
+    let offsetTop = this.canvas.clientHeight * 0.25;
+    let beatHeight = this.canvas.clientHeight * 0.5;
     for (let i = 0; i < validBeats.length; i++) {
       var beat = validBeats[i];
       let node;
@@ -82,8 +85,6 @@ class Timeline {
       } else {
         node = document.createElement('div');
         node.style.width = '8px';
-        node.style.height = '300px';
-        node.style.top = '100px';
         node.style.backgroundColor = '#FF0000';
         node.style.position = "absolute";
         node.style.borderRadius = '4px';
@@ -91,6 +92,8 @@ class Timeline {
       }
       const x = ((beat - left) / this.windowSize) * this.canvas.clientWidth;
       node.style.left = `${x - 4}px`;
+      node.style.top = `${offsetTop}px`;
+      node.style.height = `${beatHeight}px`;
       if (!node.parentNode) {
         this.canvasOverlay.appendChild(node);
       }
@@ -114,6 +117,14 @@ class Timeline {
     return this.curTime;
   }
 
+  currentTimeString() {
+    const tmp = new Date(this.curTime * 1000);
+    const minutes = tmp.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = tmp.getSeconds().toString().padStart(2, '0');
+    const millis = tmp.getMilliseconds().toString().padStart(3, '0');
+    return `${minutes}:${seconds}:${millis}`;
+  }
+
   reset() {
     this.curTime = 0;
   }
@@ -130,12 +141,12 @@ class Timeline {
     this.curTime = time;
   }
 
-  render(timeMS) {
+  render(timeMS, beats) {
     if (this.active) {
       this.curTime += (timeMS - this.lastFrameTimeMS) / 1000;
     }
     this.drawTicks(this.curTime);
-    this.drawBeats(this.curTime);
+    this.drawBeats(this.curTime, beats);
     this.lastFrameTimeMS = timeMS;
   }
 }
