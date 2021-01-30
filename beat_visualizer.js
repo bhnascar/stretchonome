9b5254ab82c2244c2f830f8f23624db70a9d2679
bhnascar <bhnascar@gmail.com>
2021-01-30 13:17:12 -0800

    Cleanup unused method.

--- beat_visualizer.js
+++ beat_visualizer.js
@@ -58,22 +58,4 @@ class BeatVisualizer {
     }
     this.activeNodes = currentNodes;
   }
-
-  binarySearchGE(arr, target) {
-    let start = 0;
-    let end = arr.length - 1;
-
-    while (start <= end) {
-      const mid = Math.floor((start + end) / 2);
-      if (arr[mid] >= target && (mid == 0 || arr[mid - 1] < target)) {
-        return mid;
-      }
-      if (arr[mid] < target) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
-    }
-    return -1;
-  }
 }
